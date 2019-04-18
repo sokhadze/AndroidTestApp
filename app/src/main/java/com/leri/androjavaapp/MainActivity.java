947cea47d8994b9ff2d562e431d54d1eb8e189a5
sokhadze <lerisokhadze@gmail.com>
2019-04-18 18:16:08 +0400

    upload project

---
+++ app/src/main/java/com/leri/androjavaapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.leri.androjavaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
