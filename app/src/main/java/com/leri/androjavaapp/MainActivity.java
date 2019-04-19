4f0fc90acaef4b4ef4af30059c8599dcca798895
sokhadze <lerisokhadze@gmail.com>
2019-04-19 18:19:29 +0400

    changed bg color project

--- app/src/main/java/com/leri/androjavaapp/MainActivity.java
+++ app/src/main/java/com/leri/androjavaapp/MainActivity.java
@@ -2,12 +2,32 @@ package com.leri.androjavaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.login);
+
+        final TextView signIn = (TextView) findViewById(R.id.textView);
+
+        nextBtn = (Button) findViewById(R.id.nextBtn);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn.setText("Loading...");
+            }
+        });
+
     }
+
+
+
 }
